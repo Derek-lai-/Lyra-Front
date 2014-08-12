cd928f5886d63c5b5891afc45298d03a6ea43407
Derek <derek-lai-@hotmail.com>
2014-08-12 16:46:20 -0400

    Started some JS

--- Lyra/www/js/index.js
+++ Lyra/www/js/index.js
@@ -1,4 +1,34 @@
 $(document).ready(function()) {
     
-    
+    $(.slider_menu).hide();
+    var hammertime = $(document).hammer();
+
+    var sidebar_out = false;
+    hideSidebar()
+
+    hammertime.on("swiperleft", function(ev)) {
+    	if (sidebar_out) {
+    		hideSidebar();
+    	} else if (!sidebar_out) {
+    		showSidebar();
+    	}
+    });
+
+    function hideSidebar(){
+    	$('.main').animate({
+    		left: '0'
+    	}, 250, function() { 
+    		sidebar_out = false;
+    		$('.menu').hide()
+		});
+    }
+
+    $('.sidebar_toggle').click(function(){
+    	if (!sidebar_out) {
+    		showSidebar();
+    	} else {
+    		hideSidebar();
+    	}
+    });
+
 }
