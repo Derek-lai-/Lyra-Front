beb5b93e96ad17245735792f43ec85a0da75dccd
Derek <derek-lai-@hotmail.com>
2014-09-03 02:04:13 -0400

    Transitioning to Snap JS

--- Lyra/www/js/slide-menu.js
+++ Lyra/www/js/slide-menu.js
@@ -1,26 +1,4 @@
 $(document).ready(function() {
-	$(".m-menu").click(function(){
-	
-		$("#container").toggleClass("active");
-		$(".m-menu").toggleClass("active");
-		$(".mobile-menu").toggleClass("active");
-		$(".mobile-menu-bg").addClass("active");
-	});
-	
-	$(".mobile-menu").css('left','-36%');
-	$(".mobile-menu-bg").css('left','-36%');
-	$(".m-menu").toggle(function() {
-		$('.mobile-menu').transition({ left: '0' }, { duration: 700, queue: false });
-		$('.mobile-menu-bg').transition({ left: '0' }, { duration: 700, queue: false });
-		$('#container').transition({ 'margin-left': '36%' }, { duration: 700, queue: false });
-		$('#container').transition({ 'margin-right': '-36%' }, { duration: 700, queue: false });
-	}, function() {       
-		$('.mobile-menu-bg').transition({ left: '-36%' }, { duration: 700, queue: false });
-		$('.mobile-menu').transition({ left: '-36%' }, { duration: 700, queue: false });
-		$('#container').transition({ 'margin-left': '0' }, { duration: 700, queue: false });
-		$('#container').transition({ 'margin-right': '0' }, { duration: 700, queue: false });
-	}
-	);
 
 	var shiftleft = false;
 	var shiftright = false;
@@ -54,8 +32,13 @@ $(document).ready(function() {
 
 	var listorder = ("alpha", "bravo", "charlie", "delta", "echo");
 
-	$$('.cursong').tap(function(){
-		//$('#background').addClass('open');
+	var snapper = new Snap({
+		element: document.getElementById('content')
+	});
+
+
+	$$('.cursong').swipeLeft(function(){
+		
 	});
 
 	$$('.cursong').swipeRight(function(){
