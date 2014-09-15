48d33b7efc9ee3df51901e731999bd6a6887fbb8
Derek <derek-lai-@hotmail.com>
2014-09-15 17:20:37 -0400

    adding javascript

--- lyra/www/js/main.js
+++ lyra/www/js/main.js
@@ -3,10 +3,52 @@ $(document).ready(appReady);
 function appReady() {
 
 	var snapper = new Snap({
-		element: document.getElementById("content")
+		element: document.getElementById("content"),
+		touchToDrag: false,
+		disable: "right"
+	});
+
+	$(document).on("click", "#toolbar-menu", function(){
+		snapper.open("left")
+	});
+
+	snapper.on("animated", function(){
+		$("body").width("0%").width("100%");
+	});
+
+	var currPage = "";
+	var pageStack = new Array();
+
+
+	$$(document).on("tap", ".side-link", function(event){
+		event.stopPropagation();
+		event.preventDefault();
+		if (event.handled !== true) {
+			changePage($(this).data("page"))
+			event.handled = true;
+		} else {
+			return false;
+		}
 	})
 
+	function changePage(page) {
+		snapper.close();
 
+		if (page === "back") {
+			page = pageStack.pop();
+		}
+
+		$(".page").each(function(){
+			if ($(this).data("page") == page) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+
+
+
+		})
+	}
 
 
 }
