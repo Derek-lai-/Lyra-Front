cd7201f2e91065c49668f365d3d075f4b266ae25
derek-lai- <derek_lai_@hotmail.com>
2014-12-01 03:01:13 -0500

    Lots of fixes to be made, Camra capture fix inprogress, no crash but doesnt start

--- Lyra/app/src/main/java/com/example/lyra/lyra/MainMenuActivity.java
+++ Lyra/app/src/main/java/com/example/lyra/lyra/MainMenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.lyra.lyra;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -31,21 +32,32 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import javax.xml.datatype.Duration;
+
 
 public class MainMenuActivity extends Activity {
     private static final String TAG = "CallCamera";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int CAPTURE_IMAGE_ACTIVITY_REQ = 0;
     private WebSocketClient mWebSocketClient;
+
+    private Button camera;
     private Button playSong;
     private Button stopSong;
+    private Button btnSongs;
+
     private SeekBar seekBar;
     private MediaPlayer mediaPlayer;
     private TextView songName;
 
+    private ImageView photoImage;
+    private Bitmap imageBitmap;
+
     private boolean Complete;
 
-    Uri fileUri = null;
-    ImageView photoImage = null;
+    private String currentPhotoPath;
+
+    private Uri fileUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -53,9 +65,14 @@ public class MainMenuActivity extends Activity {
         setContentView(R.layout.activity_fullscreen);
 
         photoImage = (ImageView) findViewById(R.id.photo_img);
+
+        camera = (Button) findViewById(R.id.capture);
         playSong = (Button) findViewById(R.id.btnPlay);
         stopSong = (Button) findViewById(R.id.btnPause);
+        btnSongs = (Button) findViewById(R.id.songs);
+
         seekBar = (SeekBar) findViewById(R.id.seekBar);
+
         songName = (TextView) findViewById(R.id.songName);
 
         mediaPlayer = new MediaPlayer();
@@ -67,16 +84,12 @@ public class MainMenuActivity extends Activity {
             }
         });
 
-        photoImage.setImageDrawable(null);
-        Button btnCamera = (Button) findViewById(R.id.capture);
-
-        btnCamera.setOnClickListener(new View.OnClickListener() {
+        camera.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 cameraAct();
             }
         });
 
-        Button btnSongs = (Button) findViewById(R.id.songs);
         btnSongs.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent i = new Intent(MainMenuActivity.this, LibraryActivity.class);
@@ -85,64 +98,55 @@ public class MainMenuActivity extends Activity {
         });
     }
 
+    /* ---------------------------------- Photo Act Begins --------------------------------------*/
     private void cameraAct(){
-        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File file = getOutputPhotoFile();
-        fileUri = Uri.fromFile(getOutputPhotoFile());
-        i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-        i.putExtra("android.intent.extras.CAMERA_FACING", 1);
-        startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQ);
-    }
-
-
-    private File getOutputPhotoFile(){
-        File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), getPackageName());
-        if (!directory.exists()){
-            if (!directory.mkdirs()){
-                Log.e(TAG, "Failed to create storage directory.");
-                return null;
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (pictureIntent.resolveActivity(getPackageManager()) != null){
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                currentPhotoPath = photoFile.getAbsolutePath();
+                pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+                photoFile = null;
+                currentPhotoPath = null;
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
-        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.CANADA).format(new Date());
-        return new File(directory.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
     }
 
-    private void showPhoto(Uri photoUri){
-        File imageFile = new File(photoUri.getPath());
-        if (imageFile.exists()){
-            Drawable oldDrawable = photoImage.getDrawable();
-            if (oldDrawable != null){
-                ((BitmapDrawable) oldDrawable).getBitmap().recycle();
-            }
-            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-            BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmap);
-            photoImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            photoImage.setImageDrawable(drawable);
-        }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg",        /* suffix */
+                storageDir     /* directory */
+        );
 
+        return image;
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQ) {
-            if (resultCode == RESULT_OK){
-                Uri photoUri = null;
-                if (data == null) {
-                    // bug here
-                    Toast.makeText(this, "Image saved successfully", Toast.LENGTH_SHORT).show();
-                    photoUri = fileUri;
-                } else {
-                    photoUri = data.getData();
-                    Toast.makeText(this, "Imaged saved sucessfully in : " + data.getData(), Toast.LENGTH_LONG).show();
-                }
-                showPhoto(photoUri);
-            } else if (resultCode == RESULT_CANCELED){
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Image capture failed!", Toast.LENGTH_LONG).show();
-            }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            photoImage.setImageBitmap(imageBitmap);
+            photoImage.setVisibility(View.INVISIBLE);
         }
     }
+    /* ----------------------------------- Photo Act Ends ---------------------------------------*/
 
+    /* --------------------------------- Song Controls Beings -----------------------------------*/
     @Override
     protected void onDestroy(){
         super.onDestroy();
@@ -179,6 +183,7 @@ public class MainMenuActivity extends Activity {
         Complete=true;
     }
 
+    /* --------------------------------- Song Controls Ends -------------------------------------*/
 
     private void connectWebSocket() {
         URI uri;
