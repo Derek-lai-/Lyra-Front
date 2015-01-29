0e43b711685310e5c733b5da834d50d697adf16d
Derek-lai- <derek_lai_@hotmail.com>
2015-01-29 14:23:50 -0500

    develop to master

---
+++ Lyra/app/src/main/java/com/example/lyra/lyra/SongControls.java
@@ -0,0 +1,21 @@
+package com.example.lyra.lyra;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
+
+/**
+ * Created by derek on 11/12/14.
+ */
+public class SongControls{
+    @Override
+    public void onRecieve(Context context, Intent intent)
+
+    }
+
+
+
+
+
+}
